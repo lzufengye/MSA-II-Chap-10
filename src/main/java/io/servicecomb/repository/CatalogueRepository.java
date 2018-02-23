9197dbed507c13b795ca64413067765ebb35741f
unknown <w00402903@szxbz1498.huaweiobz.com>
2018-02-23 17:50:54 +0800

    catalogue service

---
+++ src/main/java/io/servicecomb/repository/CatalogueRepository.java
@@ -0,0 +1,11 @@
+package io.servicecomb.repository;
+
+import io.servicecomb.model.Catalogue;
+
+import java.util.List;
+
+public class CatalogueRepository {
+    public List<Catalogue> all() {
+        return null;
+    }
+}
